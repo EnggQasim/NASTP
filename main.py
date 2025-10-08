decad7730a4a94302ffa5358c97fc7bfae6a371b
Sir Qasim <m.qasim077@gmail.com>
2025-10-08 13:14:02 +0500

    final1

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ async def read_root():
     return "Pakistan zindabad!"
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9001)
+    uvicorn.run(app, host="0.0.0.0", port=5008)
